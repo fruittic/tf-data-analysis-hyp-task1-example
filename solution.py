01f4af6fe5b7ed4dba3dfaa8a19d46cc1c457e70
fruittic <129435665+fruittic@users.noreply.github.com>
2023-04-10 22:10:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 224851402 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  alpha = 0.07
+  count = np.array([x_success, y_success])
+  nobs = np.array([x_cnt, y_cnt])
+  zstat, p_value = proportions_ztest(count=count, nobs=nobs, alternative='larger')
+  return p_value < alpha
